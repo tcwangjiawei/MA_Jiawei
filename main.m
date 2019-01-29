942d547c2607a770e196c9367825b23ee40ead51
Jiawei Wang <43844445+Jiaweiscu@users.noreply.github.com>
2019-01-29 15:16:39 +0100

    Delete main.m

--- main.m
+++
@@ -1,8 +0,0 @@
-
-clear; close all; clc
-addpath('./Data');
-
-run slidingwindow_lanechangright.m;
-run slidingwindow_lanechangleft.m;
-run slidingwindow_lanekeep1.m;
-run slidingwindow_lanekeep2.m;
